ad8007d72fe5f291bd081e6550c9225addfb30d7
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2025-03-27 14:24:19 +0430

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,19 +1,18 @@
 {
-    "version": 2,
-    "builds": [
-        {
-            "src": "api/bot.js",
-            "use": "@vercel/node"
-        }
-    ],
-    "routes": [
-        {
-            "src": "/api/bot",
-            "dest": "/api/bot.js"
-        }
-    ],
-    "env": {
-        "TELEGRAM_TOKEN": "@telegram-token",
-        "VERCEL_URL": "@vercel-url"
+  "version": 2,
+  "builds": [
+    {
+      "src": "api/index.js",
+      "use": "@vercel/node"
     }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/api/index.js"
+    }
+  ],
+  "env": {
+    "BOT_TOKEN": "@youtube-downloader-bot-token"
+  }
 }
